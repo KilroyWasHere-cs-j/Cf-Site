65d943e1280aa90b97b59502373043af76ea3249
Kilroy <gabriel.tower@baxter-academy.org>
2023-05-10 10:16:48 -0400

    Change

--- src/lib/PageHeader.svelte
+++ src/lib/PageHeader.svelte
@@ -12,6 +12,6 @@
         <p class="text-5sm font-bold text-center">{subheader}</p>
         <p class="text-3sm font-bold text-center">{subtext}</p>
         <div class="w-24 h-1 bg-blue-500 mt-4"></div>
-        <p class="text-2sm font-bold text-center">B1</p>
+        <p class="text-xs font-bold text-center">B1</p>
     </div>
 </div>
