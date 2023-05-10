101a9a2a20edd143cd9c47846309ded6de489a5f
Kilroy <gabriel.tower@baxter-academy.org>
2023-05-10 10:03:06 -0400

    Added build ID in header

--- src/lib/PageHeader.svelte
+++ src/lib/PageHeader.svelte
@@ -8,8 +8,10 @@
 <div class="flex flex-col w-screen mt-2 items-center justify-center px-6">
     <div class="flex flex-col items-center justify-center px-6">
         <h1 class="text-5xl font-bold text-center">{header}</h1>
-        <p class="text-5sl font-bold text-center">{subheader}</p>
-        <p class="text-3sl font-bold text-center">{subtext}</p>
+        <div class="w-24 h-1 mt-4"></div>
+        <p class="text-5sm font-bold text-center">{subheader}</p>
+        <p class="text-3sm font-bold text-center">{subtext}</p>
         <div class="w-24 h-1 bg-blue-500 mt-4"></div>
+        <p class="text-2sm font-bold text-center">B1</p>
     </div>
 </div>
